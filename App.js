b3f470b3d0d989a2be011bc5dab866290dbc0bd6
Mike-Andrew <Mike-Andrew@users.noreply.github.com>
2020-03-11 14:43:51 -0500

    fixing links to videos

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { Video } from './Video';
 import { Menu } from './Menu';
 
 const VIDEOS = {
-  fast: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4',
-  slow: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4',
-  cute: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4',
-  eek: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4'
+  fast: './videos/react_video-fast.mp4',
+  slow: './videos/react_video-slow.mp4',
+  cute: './videos/react_video-cute.mp4',
+  eek: './videos/react_video-eek.mp4'
 };
 
 class App extends React.Component {
